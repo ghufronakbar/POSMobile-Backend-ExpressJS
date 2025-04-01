86a7dc5ebc5bbcf038298af5d626e158806239b5
ghufron akbar <alan.040219.gam@gmail.com>
2025-04-02 03:19:12 +0700

    Add Order Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@ import account from '../services/account.js';
 import user from '../services/user.js';
 import products from '../services/product.js';
 import image from '../services/image.js'
+import order from '../services/order.js';
 
 router.use('/account', account);
 router.use('/users', user);
 router.use('/products', products);
+router.use('/orders', order)
 router.use("/image", image)
 
 export default router
