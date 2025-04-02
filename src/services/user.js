f50092741692bd46e5563cae4c9d40a6458d7e1d
ghufron akbar <alan.040219.gam@gmail.com>
2025-04-02 12:17:31 +0700

    Add New Case User

--- src/services/user.js
+++ src/services/user.js
@@ -2,6 +2,8 @@ import express from 'express'
 import prisma from '../db/prisma.js'
 import { sendEmail } from '../utils/node-mailer/send-email.js';
 import verification from '../middleware/verification.js';
+import randomCharacter from '../utils/randomCharacter.js';
+import bcrypt from 'bcryptjs'
 const router = express.Router()
 
 const getAllUsers = async (req, res) => {
@@ -75,10 +77,112 @@ const deleteUser = async (req, res) => {
     }
 }
 
+const createUser = async (req, res) => {
+    const { name, email, countryCode, phone, address, role, image } = req.body
+    if (!name || !email || !countryCode || !phone || !address || !role) {
+        return res.status(400).json({ status: 400, message: 'Harap isi semua field' })
+    }
+
+    if (role !== "Admin" && role !== "Employee") {
+        return res.status(400).json({ status: 400, message: 'Role tidak valid' })
+    }
+
+    if (!validateEmail(email)) {
+        return res.status(400).json({ status: 400, message: 'Email tidak valid' })
+    }
+    try {
+        const checkEmail = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
+        if (checkEmail) {
+            return res.status(400).json({ status: 400, message: "Email sudah terdaftar" })
+        }
+        const password = randomCharacter(8)
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const send = await sendEmail(email, "CREATE_ACCOUNT", name, password)
+        if (send instanceof Error) {
+            return res.status(500).json({ status: 500, message: 'Gagal mengirim email' })
+        }
+        const user = await prisma.user.create({
+            data: {
+                address,
+                countryCode,
+                email,
+                name,
+                password: hashedPassword,
+                phone,
+                role,
+                image: image || null
+            }
+        })
+
+        return res.status(200).json({ message: "Berhasil membuat akun!", data: user })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ status: 500, message: 'Terjadi Kesalahan Sistem!' })
+    }
+}
+
+const editUser = async (req, res) => {
+    const { id } = req.params
+    const { name, email, countryCode, phone, address, role, image } = req.body
+    if (!name || !email || !countryCode || !phone || !address || !role) {
+        return res.status(400).json({ status: 400, message: 'Harap isi semua field' })
+    }
+
+    if (role !== "Admin" && role !== "Employee") {
+        return res.status(400).json({ status: 400, message: 'Role tidak valid' })
+    }
+
+    if (!validateEmail(email)) {
+        return res.status(400).json({ status: 400, message: 'Email tidak valid' })
+    }
+    try {
+        const checkEmail = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
+        if (checkEmail && checkEmail.id !== id) {
+            return res.status(400).json({ status: 400, message: "Email sudah terdaftar" })
+        }
+        const password = randomCharacter(8)
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const send = await sendEmail(email, "CREATE_ACCOUNT", name, password)
+        if (send instanceof Error) {
+            return res.status(500).json({ status: 500, message: 'Gagal mengirim email' })
+        }
+        const user = await prisma.user.update({
+            data: {
+                address,
+                countryCode,
+                email,
+                name,
+                password: hashedPassword,
+                phone,
+                role,
+                image: image || null
+            },
+            where: [
+                id
+            ]
+        })
+
+        return res.status(200).json({ message: "Berhasil membuat akun!", data: user })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ status: 500, message: 'Terjadi Kesalahan Sistem!' })
+    }
+}
+
 
 
 router.get("/", verification(["Admin", "Employee"]), getAllUsers)
 router.get("/:id", verification(["Admin", "Employee"]), getUser)
+router.post("/", verification(["Admin", "Employee"]), createUser)
+router.put("/:id", verification(["Admin", "Employee"]), editUser)
 router.delete("/:id", verification(["Admin", "Employee"]), deleteUser)
 
 export default router
